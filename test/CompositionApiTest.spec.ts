ea9ce97b5428abc9937fa8f4b47d656014b13dca
James Wu <james_wu@appdet.com>
2021-01-24 14:50:59 +0800

    test unit test

--- test/CompositionApiTest.spec.ts
+++ test/CompositionApiTest.spec.ts
@@ -1,18 +1,13 @@
 import { shallowMount } from '@vue/test-utils'
 // import Pages from '@/components/Logo.vue'
-import Pages from '@/pages/index.vue'
+import Pages from '@/pages/_test/index.vue'
+
 
 
 describe('Composition Api Unit Test', () => {
-  const wrapper = shallowMount(Pages, {
-    data () {
-      return {
-        title: 'Composition Api Unit Test - Use Jest'
-      }
-    }
-  })
+  const wrapper = shallowMount(Pages)
 
-  test('set title data', () => {
-    expect(wrapper.find('.title').text()).toMatch('Composition Api Unit Test - Use Jest')
+  test('router test', () => {
+    
   })
 })
