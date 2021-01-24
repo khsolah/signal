647bab092d81981de5b2a1f033f6d77b1eb7a385
James Wu <james_wu@appdet.com>
2021-01-24 14:48:33 +0800

    home

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,15 @@
 <template>
   <div>
-    <p class="title">{{ title }}</p>
     <Header />
+    <Carousel />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from '@nuxtjs/composition-api'
-import Header from '@/components/Header/index.vue'
 
 export default defineComponent({
   setup() {
-    const title = ref('Welcome to Nuxt Typescript')
-
-    return { title }
-  }
+  },
 })
 </script>
