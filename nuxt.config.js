69ecd58954be630107662a2573672920a4ef58a8
James Wu <james_wu@appdet.com>
2021-01-24 14:50:21 +0800

    config

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,10 +16,14 @@ export default {
   loading: { color: 'palevioletred', height: '1.5px', duration: 5000 },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '~/assets/scss/main.scss'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    // { src: '~/plugins/flickity.js', mode: 'client' }
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
