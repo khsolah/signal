302e81b51e49c2207dd65c3e46de849016a86ab5
James Wu <james_wu@appdet.com>
2021-02-09 20:53:14 +0800

    + store:index

---
+++ store/index.ts
@@ -0,0 +1,41 @@
+import { ActionTree, GetterTree, MutationTree } from 'vuex'
+
+export type RootState = ReturnType<typeof state>
+
+export const state = () => ({
+  WindowWidth: 0,
+  WindowSize: ''
+})
+
+export const getters: GetterTree<RootState, RootState> = {
+  WindowWidth: state => state.WindowWidth
+}
+
+export const mutations: MutationTree<RootState> = {
+  DetectWindowResize: (state) => {
+    console.log('resize')
+    state.WindowWidth = window.innerWidth
+    switch (true) {
+      case (window.innerWidth < 576):
+        state.WindowSize = 'xs'
+        break
+      case (window.innerWidth < 768):
+        state.WindowSize = 'sm'
+        break
+      case (window.innerWidth < 992):
+        state.WindowSize = 'md'
+        break
+      case (window.innerWidth < 1200):
+        state.WindowSize = 'lg'
+        break
+      default:
+        state.WindowSize = 'xl'
+    }
+  }
+}
+
+export const actions: ActionTree<RootState, RootState> = {
+  DetectWindowResize: ({ commit }) => {
+    commit('DetectWindowResize')
+  },
+}
