379243ca8a0b3261ae3642611ef4728c2d5562be
James Wu <james_wu@appdet.com>
2021-02-12 22:15:40 +0800

    + new page: collections

---
+++ pages/collections.vue
@@ -0,0 +1,23 @@
+<template>
+  <article id="collection">
+    <!-- <SideMenu /> -->
+    <b-container>
+      <b-row></b-row>
+    </b-container>
+    <ProductsList />
+  </article>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'Collections',
+  setup (prop, { root }) {
+    
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
