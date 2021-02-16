b8c842e0fc8e57654ce4e21efaa932b15621a34c
James Wu <james_wu@appdet.com>
2021-02-16 19:07:20 +0800

    + product descriptions page

---
+++ pages/collections/_category/_products.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <ProductCarousel :product="ProductCarousel" />
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'ProductPage',
+  setup (prop, { root }) {
+    const ProductCarousel: ImageData[] = [
+      require('@/assets/images/carousel-banner/gmk-amethyst.png'),
+      require('@/assets/images/carousel-banner/gmk-dots.jpg'),
+      require('@/assets/images/carousel-banner/gmk-posh.jpg'),
+      require('@/assets/images/carousel-banner/gmk-rudy.png'),
+    ]
+
+    return {
+      ProductCarousel
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
