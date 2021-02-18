dfb7533cc9056f09b4d80ebd5867c4f693ff88e4
James Wu <james_wu@appdet.com>
2021-02-18 10:04:17 +0800

    + [add to cart] and [remove from cart] function in account module.

--- store/account.ts
+++ store/account.ts
@@ -1,11 +1,14 @@
-import { GetterTree } from "vuex/types/index"
+import { ActionTree, GetterTree, MutationTree } from "vuex/types/index"
 import { RootState } from "."
+
+import { ICartProduct } from '~/common/interface'
 
 export const state = () => ({
   id: '',
   name: '',
   token: '',
-  legalToken: false
+  legalToken: false,
+  cart: [] as ICartProduct[]
 })
 
 export type AccountState = ReturnType<typeof state>
@@ -16,5 +19,29 @@ export const getters: GetterTree<AccountState, RootState> = {
     name: state.name,
     isLogin: state.token,
     legalToken: state.legalToken
-  })
+  }),
+  ShowCart: state => state.cart
+}
+
+export const mutations: MutationTree<AccountState> = {
+  AddProductToCart: (state, payload: ICartProduct) => {
+    const index = state.cart.findIndex(el => el.name === payload.name)
+    if (index !== -1) {
+      state.cart[index].amount++
+    } else {
+      state.cart.push(payload)
+    }
+  },
+  RemoveProductFromCart: (state, name: string):void => {
+    state.cart.splice(state.cart.findIndex(el => el.name === name), 1)
+  }
+}
+
+export const actions: ActionTree<AccountState, RootState> = {
+  AddProductToCart: ({ commit }, payload: ICartProduct) => {
+    commit('AddProductToCart', payload)
+  },
+  RemoveProductFromCart: ({ commit }, name: string) => {
+    commit('RemoveProductFromCart', name)
+  },
 }
