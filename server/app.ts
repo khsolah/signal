3b0770b88296197f20d67f8002c780950ea31670
James Wu <james_wu@appdet.com>
2021-03-01 21:08:17 +0800

    * adjust port - test code

--- server/app.ts
+++ server/app.ts
@@ -1,17 +1,17 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
+
 const app = express()
 
-import product from './routes/product.routes'
+import routes from './routes/index'
 
 app.use(bodyParser.json())
-app.get('/', (req, res) => {
-  res.send('this is api page')
-})
+app.use(cors)
 
 // app.use(product)
 
-app.use('/apitest', product)
+app.use('/api', routes)
 app.listen(4000, () => {
   console.log('port 4000')
 })
