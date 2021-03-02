f31e935d1ce6c27c370fa6a3a8d1b3985eed2c85
James Wu <james_wu@appdet.com>
2021-03-02 09:23:07 +0800

    - test code

--- server/routes/home.routes.ts
+++ server/routes/home.routes.ts
@@ -3,9 +3,6 @@ import controllers from '../controllers/index.controllers'
 
 const router = express.Router()
 
-// router.get('/', (req, res) => {
-//   res.send('now in api/home page')
-// })
 router.route('/').get(controllers.get)
 
 export default router
