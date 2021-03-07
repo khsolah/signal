24797c1231c9e4a3ac547393070052032c4e3fed
James Wu <james_wu@appdet.com>
2021-03-07 15:58:33 +0800

    * set get product controller

--- server/controllers/product.controllers.ts
+++ server/controllers/product.controllers.ts
@@ -1,18 +1,31 @@
 import { Request, Response } from 'express'
-import { IProductAbstract } from '../../common/interface'
+import { IProduct, IProductAbstract } from '../../common/interface'
 import modules from '../modules/product.modules'
 
 const get = (req: Request, res: Response) => {
-  modules.get((req.query.category) as string|undefined).then((response: IProductAbstract[]) => {
-    res.json(response)
-  }).catch((error: Error) => {
-    console.error(error)
-    res.status(500)
-    res.statusMessage = 'An internal error has occurred!'
-    res.send()
-  })
+  modules
+    .get(
+      req.query.category as string | undefined,
+      req.query.product as string | undefined
+    )
+    .then((response: IProductAbstract[] | IProduct) => {
+      res.json(response)
+    })
+    .catch(
+      ({
+        status,
+        statusMessage,
+      }: {
+        status: number
+        statusMessage: string
+      }) => {
+        res.status(status)
+        res.statusMessage = statusMessage
+        res.send()
+      }
+    )
 }
 
 export default {
-  get
+  get,
 }
