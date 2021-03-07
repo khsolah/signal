d1685aba28120dbcb382c9a8edae9c5154dbeb86
James Wu <james_wu@appdet.com>
2021-03-07 15:56:29 +0800

    * adjust return data

--- server/modules/product.modules.ts
+++ server/modules/product.modules.ts
@@ -1,79 +1,262 @@
-import { IProductAbstract } from '../../common/interface'
+import { IProduct, IProductAbstract } from '../../common/interface'
 
-const get = (category?: string): Promise<IProductAbstract[]> => {
+const keycapsList: IProduct[] = [
+  {
+    id: '1',
+    name: 'GMK Dots',
+    price: 130,
+    images: [
+      '/images/keycaps/dots/dots-1.jpg',
+      '/images/keycaps/dots/dots-2.jpg',
+      '/images/keycaps/dots/dots-3.jpg',
+      '/images/keycaps/dots/dots-4.jpg',
+      '/images/keycaps/dots/dots-5.jpg',
+    ],
+    tag: 'group buy',
+    tagVariant: 'warning',
+    description: `<h2>THIS IS A GROUPBUY</h2>
+      <h6>
+        <b>
+          Groupbuy will run from December 6th - December 31th 2020. <br>
+          The approximate shipping time is around Q4 2021.<br>
+          Keycap Set Designed by Omnitype
+        </b>
+      </h6>
+      <h2>Details</h2>
+      <ul>
+        <li>Designed by Plop</li>
+        <li>Produced by ePBT</li>
+        <li>PBT Dyesub</li>
+        <li>Deskmats: stitched edge, 900x400 mm, 3mm thick</li>
+        <li>THOK Novelty: Aluminum, R1 profile, 2 color resin infill.</li>
+      </ul>
+      <p>The images shown are renders of the product and are only used to estimate the final product. The end product may vary in color from the renders as they are not guaranteed to be 100% accurate to the end product. By purchasing this product, you are agreeing to the above terms.</p>`,
+    buttonText: 'Join Group Buy',
+  },
+  {
+    id: '2',
+    name: 'GMK Posh',
+    price: 155,
+    images: [
+      '/images/keycaps/posh/posh-1.jpg',
+      '/images/keycaps/posh/posh-2.jpg',
+      '/images/keycaps/posh/posh-3.jpg',
+      '/images/keycaps/posh/posh-4.jpg',
+    ],
+    tag: 'group buy',
+    tagVariant: 'warning',
+    description: `<h2>THIS IS A GROUPBUY</h2>
+      <h6>
+        <b>
+          Groupbuy will run from December 6th - December 31th 2020. <br>
+          The approximate shipping time is around Q4 2021.<br>
+          Keycap Set Designed by Omnitype
+        </b>
+      </h6>
+      <h2>Details</h2>
+      <ul>
+        <li>Designed by Plop</li>
+        <li>Produced by ePBT</li>
+        <li>PBT Dyesub</li>
+        <li>Deskmats: stitched edge, 900x400 mm, 3mm thick</li>
+        <li>THOK Novelty: Aluminum, R1 profile, 2 color resin infill.</li>
+      </ul>
+      <p>The images shown are renders of the product and are only used to estimate the final product. The end product may vary in color from the renders as they are not guaranteed to be 100% accurate to the end product. By purchasing this product, you are agreeing to the above terms.</p>`,
+    buttonText: 'Join Group Buy',
+  },
+  {
+    id: '3',
+    name: 'GMK Amethyst',
+    price: 160,
+    images: [
+      '/images/keycaps/amethyst/amethyst-1.jpg',
+      '/images/keycaps/amethyst/amethyst-2.jpg',
+    ],
+    tag: 'group buy',
+    tagVariant: 'warning',
+    description: `<h2>THIS IS A GROUPBUY</h2>
+      <h6>
+        <b>
+          Groupbuy will run from December 6th - December 31th 2020. <br>
+          The approximate shipping time is around Q4 2021.<br>
+          Keycap Set Designed by Omnitype
+        </b>
+      </h6>
+      <h2>Details</h2>
+      <ul>
+        <li>Designed by Plop</li>
+        <li>Produced by ePBT</li>
+        <li>PBT Dyesub</li>
+        <li>Deskmats: stitched edge, 900x400 mm, 3mm thick</li>
+        <li>THOK Novelty: Aluminum, R1 profile, 2 color resin infill.</li>
+      </ul>
+      <p>The images shown are renders of the product and are only used to estimate the final product. The end product may vary in color from the renders as they are not guaranteed to be 100% accurate to the end product. By purchasing this product, you are agreeing to the above terms.</p>`,
+    buttonText: 'Join Group Buy',
+  },
+  {
+    id: '3',
+    name: 'GMK Rainy Day',
+    price: 160,
+    images: [
+      '/images/keycaps/rainyday/rainyday-1.png',
+      '/images/keycaps/rainyday/rainyday-2.png',
+      '/images/keycaps/rainyday/rainyday-3.png',
+    ],
+    tag: 'group buy',
+    tagVariant: 'warning',
+    description: `<h2>THIS IS A GROUPBUY</h2>
+      <h6>
+        <b>
+          Groupbuy will run from December 6th - December 31th 2020. <br>
+          The approximate shipping time is around Q4 2021.<br>
+          Keycap Set Designed by Omnitype
+        </b>
+      </h6>
+      <h2>Details</h2>
+      <ul>
+        <li>Designed by Plop</li>
+        <li>Produced by ePBT</li>
+        <li>PBT Dyesub</li>
+        <li>Deskmats: stitched edge, 900x400 mm, 3mm thick</li>
+        <li>THOK Novelty: Aluminum, R1 profile, 2 color resin infill.</li>
+      </ul>
+      <p>The images shown are renders of the product and are only used to estimate the final product. The end product may vary in color from the renders as they are not guaranteed to be 100% accurate to the end product. By purchasing this product, you are agreeing to the above terms.</p>`,
+    buttonText: 'Join Group Buy',
+  },
+]
+
+const switchesList: IProduct[] = [
+  {
+    id: '4',
+    name: 'Zealios V2 Switches (Tactile)',
+    price: 10,
+    images: [
+      '/images/switches/zealios/zealios-1.webp',
+      '/images/switches/zealios/zealios-2.webp',
+    ],
+    tag: 'group buy',
+    tagVariant: 'warning',
+    description: `<h2>THIS IS A GROUPBUY</h2>
+      <h6>
+        <b>
+          Groupbuy will run from December 6th - December 31th 2020. <br>
+          The approximate shipping time is around Q4 2021.<br>
+          Keycap Set Designed by Omnitype
+        </b>
+      </h6>
+      <h2>Details</h2>
+      <ul>
+        <li>Designed by Plop</li>
+        <li>Produced by ePBT</li>
+        <li>PBT Dyesub</li>
+        <li>Deskmats: stitched edge, 900x400 mm, 3mm thick</li>
+        <li>THOK Novelty: Aluminum, R1 profile, 2 color resin infill.</li>
+      </ul>
+      <p>The images shown are renders of the product and are only used to estimate the final product. The end product may vary in color from the renders as they are not guaranteed to be 100% accurate to the end product. By purchasing this product, you are agreeing to the above terms.</p>`,
+    buttonText: 'Join Group Buy',
+  },
+]
+
+const diyList: IProduct[] = [
+  {
+    id: '5',
+    name: 'KBD67 DIY Kits',
+    price: 230,
+    images: [
+      '/images/diy/kbd67/kbd67-1.webp',
+      '/images/diy/kbd67/kbd67-2.webp',
+      '/images/diy/kbd67/kbd67-3.webp',
+    ],
+    tag: 'group buy',
+    tagVariant: 'warning',
+    description: `<h2>THIS IS A GROUPBUY</h2>
+      <h6>
+        <b>
+          Groupbuy will run from December 6th - December 31th 2020. <br>
+          The approximate shipping time is around Q4 2021.<br>
+          Keycap Set Designed by Omnitype
+        </b>
+      </h6>
+      <h2>Details</h2>
+      <ul>
+        <li>Designed by Plop</li>
+        <li>Produced by ePBT</li>
+        <li>PBT Dyesub</li>
+        <li>Deskmats: stitched edge, 900x400 mm, 3mm thick</li>
+        <li>THOK Novelty: Aluminum, R1 profile, 2 color resin infill.</li>
+      </ul>
+      <p>The images shown are renders of the product and are only used to estimate the final product. The end product may vary in color from the renders as they are not guaranteed to be 100% accurate to the end product. By purchasing this product, you are agreeing to the above terms.</p>`,
+    buttonText: 'Join Group Buy',
+  },
+]
+
+const keyboardsList: IProduct[] = [
+  {
+    id: '6',
+    name: 'KBD67 Mechanical Keyboard',
+    price: 390,
+    images: [
+      '/images/keyboards/kbd67/kbd67-1.webp',
+      '/images/keyboards/kbd67/kbd67-2.webp',
+      '/images/keyboards/kbd67/kbd67-3.webp',
+    ],
+    tag: 'group buy',
+    tagVariant: 'warning',
+    description: `<h2>THIS IS A GROUPBUY</h2>
+      <h6>
+        <b>
+          Groupbuy will run from December 6th - December 31th 2020. <br>
+          The approximate shipping time is around Q4 2021.<br>
+          Keycap Set Designed by Omnitype
+        </b>
+      </h6>
+      <h2>Details</h2>
+      <ul>
+        <li>Designed by Plop</li>
+        <li>Produced by ePBT</li>
+        <li>PBT Dyesub</li>
+        <li>Deskmats: stitched edge, 900x400 mm, 3mm thick</li>
+        <li>THOK Novelty: Aluminum, R1 profile, 2 color resin infill.</li>
+      </ul>
+      <p>The images shown are renders of the product and are only used to estimate the final product. The end product may vary in color from the renders as they are not guaranteed to be 100% accurate to the end product. By purchasing this product, you are agreeing to the above terms.</p>`,
+    buttonText: 'Join Group Buy',
+  },
+]
+
+const get = (
+  category?: string,
+  product?: string
+): Promise<IProductAbstract[] | IProduct> => {
   return new Promise((resolve, reject) => {
+    if (product) {
+      const result = keycapsList.filter((element) => element.name === product)
+      if (result.length === 0) {
+        reject({
+          status: 404,
+          statusMessage: 'Not Found',
+        })
+      } else {
+        resolve(result[0])
+      }
+    }
+
     if (category) {
       let data: IProductAbstract[] = []
       switch (category) {
         case 'Keyboards':
-          data = [
-            {
-              id: 'gmk-dots',
-              name: 'GMK Dots',
-              price: 160,
-              image: '/images/carousel-banner/gmk-dots.jpg',
-            },
-            {
-              id: 'gmk-posh',
-              name: 'GMK Posh',
-              price: 158,
-              image: '/images/carousel-banner/gmk-posh.jpg',
-            },
-            {
-              id: 'gmk-amethyst',
-              name: 'GMK Amethyst',
-              price: 165,
-              image: '/images/carousel-banner/gmk-amethyst.png',
-            },
-            {
-              id: 'gmk-rainy-day',
-              name: 'GMK Rainy Day',
-              price: 159,
-              image: '/images/carousel-banner/gmk-rainyday.png',
-            },
-          ]
-          break
+          resolve(keyboardsList)
         case 'Keycaps':
-          data = [
-            {
-              id: 'gmk-dots-keycaps',
-              name: 'GMK Dots',
-              price: 1600,
-              image: '/images/carousel-banner/gmk-dots.jpg',
-            },
-          ]
-          break
+          resolve(keycapsList)
         case 'Switches':
-          data = [{
-            id: 'gmk-dots-switches',
-            name: 'GMK Dots',
-            price: 160,
-            image: '/images/carousel-banner/gmk-dots.jpg',
-          },]
-          break
+          resolve(switchesList)
+        case 'DIY Kits':
+          resolve(diyList)
         default:
-          data = [
-            {
-              id: 'gmk-dots-default',
-              name: 'GMK Dots',
-              price: 160,
-              image: '/images/carousel-banner/gmk-dots.jpg',
-            },
-          ]
-          break
+          resolve([...keycapsList, ...switchesList, ...diyList, ...keyboardsList])
       }
-
-      resolve(data)
-    } else {
-      resolve([
-        {
-          id: '0',
-          name: 'name',
-          subtitle: 'subtitle',
-          price: 150,
-        },
-      ])
     }
+
+    resolve([...keycapsList, ...switchesList, ...diyList, ...keyboardsList])
   })
 }
 
