215bd20b0190170d30ec35506cdd496930ed465c
James Wu <james_wu@appdet.com>
2021-03-07 16:09:47 +0800

    * adjust data

--- server/modules/product.modules.ts
+++ server/modules/product.modules.ts
@@ -128,7 +128,7 @@ const keycapsList: IProduct[] = [
 const switchesList: IProduct[] = [
   {
     id: '4',
-    name: 'Zealios V2 Switches (Tactile)',
+    name: 'Zealios V2',
     price: 10,
     images: [
       '/images/switches/zealios/zealios-1.webp',
@@ -193,7 +193,7 @@ const diyList: IProduct[] = [
 const keyboardsList: IProduct[] = [
   {
     id: '6',
-    name: 'KBD67 Mechanical Keyboard',
+    name: 'KBD67 Keyboard',
     price: 390,
     images: [
       '/images/keyboards/kbd67/kbd67-1.webp',
@@ -223,13 +223,154 @@ const keyboardsList: IProduct[] = [
   },
 ]
 
+const featuredList: IProduct[] = [
+  {
+    id: '1',
+    name: 'GMK Dots',
+    price: 130,
+    images: [
+      '/images/keycaps/dots/dots-1.jpg',
+      '/images/keycaps/dots/dots-2.jpg',
+      '/images/keycaps/dots/dots-3.jpg',
+      '/images/keycaps/dots/dots-4.jpg',
+      '/images/keycaps/dots/dots-5.jpg',
+    ],
+    tag: 'group buy',
+    tagVariant: 'warning',
+    description: `<h2>THIS IS A GROUPBUY</h2>
+      <h6>
+        <b>
+          Groupbuy will run from December 6th - December 31th 2020. <br>
+          The approximate shipping time is around Q4 2021.<br>
+          Keycap Set Designed by Omnitype
+        </b>
+      </h6>
+      <h2>Details</h2>
+      <ul>
+        <li>Designed by Plop</li>
+        <li>Produced by ePBT</li>
+        <li>PBT Dyesub</li>
+        <li>Deskmats: stitched edge, 900x400 mm, 3mm thick</li>
+        <li>THOK Novelty: Aluminum, R1 profile, 2 color resin infill.</li>
+      </ul>
+      <p>The images shown are renders of the product and are only used to estimate the final product. The end product may vary in color from the renders as they are not guaranteed to be 100% accurate to the end product. By purchasing this product, you are agreeing to the above terms.</p>`,
+    buttonText: 'Join Group Buy',
+  },
+  {
+    id: '3',
+    name: 'GMK Amethyst',
+    price: 160,
+    images: [
+      '/images/keycaps/amethyst/amethyst-1.jpg',
+      '/images/keycaps/amethyst/amethyst-2.jpg',
+    ],
+    tag: 'group buy',
+    tagVariant: 'warning',
+    description: `<h2>THIS IS A GROUPBUY</h2>
+      <h6>
+        <b>
+          Groupbuy will run from December 6th - December 31th 2020. <br>
+          The approximate shipping time is around Q4 2021.<br>
+          Keycap Set Designed by Omnitype
+        </b>
+      </h6>
+      <h2>Details</h2>
+      <ul>
+        <li>Designed by Plop</li>
+        <li>Produced by ePBT</li>
+        <li>PBT Dyesub</li>
+        <li>Deskmats: stitched edge, 900x400 mm, 3mm thick</li>
+        <li>THOK Novelty: Aluminum, R1 profile, 2 color resin infill.</li>
+      </ul>
+      <p>The images shown are renders of the product and are only used to estimate the final product. The end product may vary in color from the renders as they are not guaranteed to be 100% accurate to the end product. By purchasing this product, you are agreeing to the above terms.</p>`,
+    buttonText: 'Join Group Buy',
+  },
+  {
+    id: '5',
+    name: 'KBD67 DIY Kits',
+    price: 230,
+    images: [
+      '/images/diy/kbd67/kbd67-1.webp',
+      '/images/diy/kbd67/kbd67-2.webp',
+      '/images/diy/kbd67/kbd67-3.webp',
+    ],
+    tag: 'group buy',
+    tagVariant: 'warning',
+    description: `<h2>THIS IS A GROUPBUY</h2>
+      <h6>
+        <b>
+          Groupbuy will run from December 6th - December 31th 2020. <br>
+          The approximate shipping time is around Q4 2021.<br>
+          Keycap Set Designed by Omnitype
+        </b>
+      </h6>
+      <h2>Details</h2>
+      <ul>
+        <li>Designed by Plop</li>
+        <li>Produced by ePBT</li>
+        <li>PBT Dyesub</li>
+        <li>Deskmats: stitched edge, 900x400 mm, 3mm thick</li>
+        <li>THOK Novelty: Aluminum, R1 profile, 2 color resin infill.</li>
+      </ul>
+      <p>The images shown are renders of the product and are only used to estimate the final product. The end product may vary in color from the renders as they are not guaranteed to be 100% accurate to the end product. By purchasing this product, you are agreeing to the above terms.</p>`,
+    buttonText: 'Join Group Buy',
+  },
+  {
+    id: '4',
+    name: 'Zealios V2',
+    price: 10,
+    images: [
+      '/images/switches/zealios/zealios-1.webp',
+      '/images/switches/zealios/zealios-2.webp',
+    ],
+    tag: 'group buy',
+    tagVariant: 'warning',
+    description: `<h2>THIS IS A GROUPBUY</h2>
+      <h6>
+        <b>
+          Groupbuy will run from December 6th - December 31th 2020. <br>
+          The approximate shipping time is around Q4 2021.<br>
+          Keycap Set Designed by Omnitype
+        </b>
+      </h6>
+      <h2>Details</h2>
+      <ul>
+        <li>Designed by Plop</li>
+        <li>Produced by ePBT</li>
+        <li>PBT Dyesub</li>
+        <li>Deskmats: stitched edge, 900x400 mm, 3mm thick</li>
+        <li>THOK Novelty: Aluminum, R1 profile, 2 color resin infill.</li>
+      </ul>
+      <p>The images shown are renders of the product and are only used to estimate the final product. The end product may vary in color from the renders as they are not guaranteed to be 100% accurate to the end product. By purchasing this product, you are agreeing to the above terms.</p>`,
+    buttonText: 'Join Group Buy',
+  },
+]
+
 const get = (
   category?: string,
   product?: string
 ): Promise<IProductAbstract[] | IProduct> => {
   return new Promise((resolve, reject) => {
-    if (product) {
-      const result = keycapsList.filter((element) => element.name === product)
+    if (product && category) {
+      let result: IProduct[] = []
+      switch (category) {
+        case 'Keyboards':
+          result = keyboardsList.filter(element => element.name === product)
+          break
+        case 'Keycaps':
+          result = keycapsList.filter(element => element.name === product)
+          break
+        case 'Switches':
+          result = switchesList.filter(element => element.name === product)
+          break
+        case 'DIY Kits':
+          result = diyList.filter(element => element.name === product)
+          break
+        case 'featured':
+          result = featuredList.filter(element => element.name === product)
+          break
+      }
+
       if (result.length === 0) {
         reject({
           status: 404,
