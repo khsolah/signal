65d7f6a9de0270944dce5a78839157590dfb597c
James Wu <james_wu@appdet.com>
2021-03-07 15:56:13 +0800

    * adjust return data

--- server/modules/home.modules.ts
+++ server/modules/home.modules.ts
@@ -5,28 +5,52 @@ const get = (): Promise<Home> => {
     resolve({
       featuredProducts: [
         {
-          id: 'gmk-dots',
+          id: '1',
           name: 'GMK Dots',
-          price: 160,
-          image: '/images/carousel-banner/gmk-dots.jpg',
+          price: 130,
+          images: [
+            '/images/keycaps/dots/dots-1.jpg',
+            '/images/keycaps/dots/dots-2.jpg',
+            '/images/keycaps/dots/dots-3.jpg',
+            '/images/keycaps/dots/dots-4.jpg',
+            '/images/keycaps/dots/dots-5.jpg',
+          ],
+          tag: 'group buy',
+          tagVariant: 'warning',
         },
         {
-          id: 'gmk-posh',
-          name: 'GMK Posh',
-          price: 158,
-          image: '/images/carousel-banner/gmk-posh.jpg',
-        },
-        {
-          id: 'gmk-amethyst',
+          id: '3',
           name: 'GMK Amethyst',
-          price: 165,
-          image: '/images/carousel-banner/gmk-amethyst.png',
+          price: 160,
+          images: [
+            '/images/keycaps/amethyst/amethyst-1.jpg',
+            '/images/keycaps/amethyst/amethyst-2.jpg',
+          ],
+          tag: 'group buy',
+          tagVariant: 'warning',
         },
         {
-          id: 'gmk-rainy-day',
-          name: 'GMK Rainy Day',
-          price: 159,
-          image: '/images/carousel-banner/gmk-rainyday.png',
+          id: '4',
+          name: 'Zealios V2 Switches (Tactile)',
+          price: 10,
+          images: [
+            '/images/switches/zealios/zealios-1.webp',
+            '/images/switches/zealios/zealios-2.webp',
+          ],
+          tag: 'group buy',
+          tagVariant: 'warning',
+        },
+        {
+          id: '5',
+          name: 'KBD67 DIY Kits',
+          price: 230,
+          images: [
+            '/images/diy/kbd67/kbd67-1.webp',
+            '/images/diy/kbd67/kbd67-2.webp',
+            '/images/diy/kbd67/kbd67-3.webp',
+          ],
+          tag: 'group buy',
+          tagVariant: 'warning',
         },
       ],
     })
