cdf26736c1f27cf909657ccf4b5dc148abfab19b
James Wu <james_wu@appdet.com>
2021-03-08 00:09:07 +0800

    * try to fix: cannot use import outside module

--- server/app.ts
+++ server/app.ts
@@ -10,9 +10,8 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.use('/', routes)
-app.listen(4000, () => {
-  console.log('port 4000')
-})
-
+// app.listen(4000, () => {
+//   console.log('port 4000')
+// })
 
 export default app
