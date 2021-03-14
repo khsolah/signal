0f395c037c15b1231b0f40c3d061082607f95342
James Wu <james_wu@appdet.com>
2021-03-14 21:25:39 +0800

    - cart, use cart module

--- store/account.ts
+++ store/account.ts
@@ -1,14 +1,11 @@
 import { ActionTree, GetterTree, MutationTree } from "vuex/types/index"
 import { RootState } from "."
 
-import { ICartOfProduct } from '~/common/interface'
-
 export const state = () => ({
   id: '',
   name: '',
   token: '',
   legalToken: false,
-  cart: [] as ICartOfProduct[]
 })
 
 export type AccountState = ReturnType<typeof state>
@@ -20,28 +17,10 @@ export const getters: GetterTree<AccountState, RootState> = {
     isLogin: state.token,
     legalToken: state.legalToken
   }),
-  ShowCart: state => state.cart
 }
 
 export const mutations: MutationTree<AccountState> = {
-  AddProductToCart: (state, payload: ICartOfProduct) => {
-    const index = state.cart.findIndex(el => el.name === payload.name)
-    if (index !== -1) {
-      state.cart[index].amount++
-    } else {
-      state.cart.push(payload)
-    }
-  },
-  RemoveProductFromCart: (state, name: string):void => {
-    state.cart.splice(state.cart.findIndex(el => el.name === name), 1)
-  }
 }
 
 export const actions: ActionTree<AccountState, RootState> = {
-  AddProductToCart: ({ commit }, payload: ICartOfProduct) => {
-    commit('AddProductToCart', payload)
-  },
-  RemoveProductFromCart: ({ commit }, name: string) => {
-    commit('RemoveProductFromCart', name)
-  },
 }
